abffc2b8ad011a507e34df8a007a44744762571c
Alex <krisstalexx@gmail.com>
2024-11-05 20:01:14 +0100

    Map validation done

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: alisseye <alisseye@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/30 17:20:21 by alisseye          #+#    #+#             */
-/*   Updated: 2024/11/04 16:59:29 by alisseye         ###   ########.fr       */
+/*   Updated: 2024/11/05 19:42:28 by alisseye         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,8 +18,23 @@
 # include "get_next_line.h"
 # include <fcntl.h>
 
+typedef struct s_point
+{
+	int	x;
+	int	y;
+}	t_point;
+
+typedef struct s_valid_way
+{
+	int	collect;
+	int	exit;
+}	t_valid_way;
+
 char	**ft_load_map(char *fd);
+char	**ft_freemap(char **map);
+char	**ft_copymap(char **map);
 int		ft_validate_map(char **map);
-char	**ft_freemap(char **map, int count_lines);
+int		ft_count(char **map, char c);
+t_point	ft_find(char **map, char c);
 
 #endif
